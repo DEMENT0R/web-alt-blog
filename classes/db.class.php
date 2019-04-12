c0ae5be0b8aa20229903f2b9af346ed4205532d6
DEMENT0R <dmntr@rambler.ru>
2019-04-12 10:23:17 +1000

    tests

--- classes/db.class.php
+++ classes/db.class.php
@@ -1,29 +1,13 @@
 <?php
     class db {
         var $db_server = 'localhost';
-        var $db_user = 'root';
-        var $db_password = 'password';
+        var $db_user = 'web-alt';
+        var $db_password = 'web-alt-test';
 
-        static protected $connection = null;
-        //Подключение к БД    
-        static function init () {
-            self::$connection = mysql_connect('localhost', 'root', '');
-                    if (self::$connection) {
-                        return mysql_select_db('lp', self::$connection);
-                    }
-                    return false;
-        }
-        //Запрос к БД    
-        static function query ($query) {
-            return mysql_query($query, self::$connection);
-        }
-        //Получаем ассоциативный массив    
-        static function fetch_assoc ($result) {
-            return mysql_fetch_assoc($result);
-        }
-        //Экранируем строки при запросах
-        static function escape_string ($str) {
-            return mysql_escape_string($str);
-        }
+    $link = mysql_connect($db_server, $db_user, $db_password);
+    if (!$link) {
+        die('Ошибка соединения: ' . mysql_error());
     }
+    echo 'Успешно соединились';
+    mysql_close($link);
 ?>
