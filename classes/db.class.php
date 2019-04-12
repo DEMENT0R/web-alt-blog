c5b95d5d1b8a824ebe3a976c7f813b601102ab4e
DEMENT0R <dmntr@rambler.ru>
2019-04-12 11:00:59 +1000

    tests

--- classes/db.class.php
+++ classes/db.class.php
@@ -11,14 +11,14 @@
             //$link = mysqli_connect("127.0.0.1", "my_user", "my_password", "my_db");
 
             if (!$link) {
-                echo "\nОшибка: Невозможно установить соединение с MySQL." . PHP_EOL;
-                echo "\nКод ошибки errno: " . mysqli_connect_errno() . PHP_EOL;
-                echo "\nТекст ошибки error: " . mysqli_connect_error() . PHP_EOL;
+                echo "<br>Ошибка: Невозможно установить соединение с MySQL." . PHP_EOL;
+                echo "<br>Код ошибки errno: " . mysqli_connect_errno() . PHP_EOL;
+                echo "<br>Текст ошибки error: " . mysqli_connect_error() . PHP_EOL;
                 exit;
             }
 
-            echo "\nСоединение с MySQL установлено!" . PHP_EOL;
-            echo "\nИнформация о сервере: " . mysqli_get_host_info($link) . PHP_EOL;
+            echo "<br>Соединение с MySQL установлено!" . PHP_EOL;
+            echo "<br>Информация о сервере: " . mysqli_get_host_info($link) . PHP_EOL;
 
             mysqli_close($link);
         }
--- index.php
+++ index.php
@@ -3,7 +3,12 @@
 //подключаем классы
 include 'classes/db.class.php';
 
-$db = new db();
+        var $db_server = 'localhost';
+        var $db_name = 'web-alt';
+        var $db_user = 'web-alt';
+        var $db_password = 'web-alt-test';
+        
+$db = new db($db_server, $db_user, $db_password, $db_name);
 $db->db_connect();
 
 ?>
