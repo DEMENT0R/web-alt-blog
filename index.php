113b267dfdbf4ecc62e603506559abb18331ebb3
DEMENT0R <dmntr@rambler.ru>
2019-04-12 11:01:28 +1000

    Update index.php

--- index.php
+++ index.php
@@ -3,11 +3,11 @@
 //подключаем классы
 include 'classes/db.class.php';
 
-        var $db_server = 'localhost';
-        var $db_name = 'web-alt';
-        var $db_user = 'web-alt';
-        var $db_password = 'web-alt-test';
-        
+$db_server = 'localhost';
+$db_name = 'web-alt';
+$db_user = 'web-alt';
+$db_password = 'web-alt-test';
+
 $db = new db($db_server, $db_user, $db_password, $db_name);
 $db->db_connect();
 
