1677ba46f597b027776ff5a31ede737f2845f670
DEMENT0R <dmntr@rambler.ru>
2019-04-12 10:49:40 +1000

    Update db.class.php

--- classes/db.class.php
+++ classes/db.class.php
@@ -5,18 +5,21 @@
         var $db_user = 'web-alt';
         var $db_password = 'web-alt-test';
 
-    $link = mysqli_connect($db_server, $db_user, $db_password, $db_name);
-    //$link = mysqli_connect("127.0.0.1", "my_user", "my_password", "my_db");
 
-    if (!$link) {
-        echo "Ошибка: Невозможно установить соединение с MySQL." . PHP_EOL;
-        echo "Код ошибки errno: " . mysqli_connect_errno() . PHP_EOL;
-        echo "Текст ошибки error: " . mysqli_connect_error() . PHP_EOL;
-        exit;
-    }
+        function db_connect {
+            $link = mysqli_connect($db_server, $db_user, $db_password, $db_name);
+            //$link = mysqli_connect("127.0.0.1", "my_user", "my_password", "my_db");
 
-    echo "Соединение с MySQL установлено!" . PHP_EOL;
-    echo "Информация о сервере: " . mysqli_get_host_info($link) . PHP_EOL;
+            if (!$link) {
+                echo "Ошибка: Невозможно установить соединение с MySQL." . PHP_EOL;
+                echo "Код ошибки errno: " . mysqli_connect_errno() . PHP_EOL;
+                echo "Текст ошибки error: " . mysqli_connect_error() . PHP_EOL;
+                exit;
+            }
 
-    mysqli_close($link);
+            echo "Соединение с MySQL установлено!" . PHP_EOL;
+            echo "Информация о сервере: " . mysqli_get_host_info($link) . PHP_EOL;
+
+            mysqli_close($link);
+        }
 ?>
