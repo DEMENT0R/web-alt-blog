09ca46857656d86a57c879575875b1dccda03c52
DEMENT0R <dmntr@rambler.ru>
2019-04-12 10:56:37 +1000

    Update db.class.php

--- classes/db.class.php
+++ classes/db.class.php
@@ -11,14 +11,14 @@
             //$link = mysqli_connect("127.0.0.1", "my_user", "my_password", "my_db");
 
             if (!$link) {
-                echo "Ошибка: Невозможно установить соединение с MySQL." . PHP_EOL;
-                echo "Код ошибки errno: " . mysqli_connect_errno() . PHP_EOL;
-                echo "Текст ошибки error: " . mysqli_connect_error() . PHP_EOL;
+                echo "\nОшибка: Невозможно установить соединение с MySQL." . PHP_EOL;
+                echo "\nКод ошибки errno: " . mysqli_connect_errno() . PHP_EOL;
+                echo "\nТекст ошибки error: " . mysqli_connect_error() . PHP_EOL;
                 exit;
             }
 
-            echo "Соединение с MySQL установлено!" . PHP_EOL;
-            echo "Информация о сервере: " . mysqli_get_host_info($link) . PHP_EOL;
+            echo "\nСоединение с MySQL установлено!" . PHP_EOL;
+            echo "\nИнформация о сервере: " . mysqli_get_host_info($link) . PHP_EOL;
 
             mysqli_close($link);
         }
