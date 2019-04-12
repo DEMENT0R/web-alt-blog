1551a4f491632d590f1e04d915b7fd98c0db4914
DEMENT0R <dmntr@rambler.ru>
2019-04-12 10:34:35 +1000

    Update db.class.php

--- classes/db.class.php
+++ classes/db.class.php
@@ -1,13 +1,22 @@
 <?php
     class db {
         var $db_server = 'localhost';
+        var $db_name = 'web-alt';
         var $db_user = 'web-alt';
         var $db_password = 'web-alt-test';
 
-    $link = mysql_connect($db_server, $db_user, $db_password);
+    $link = mysqli_connect($db_server, $db_user, $db_password, $db_name);
+    //$link = mysqli_connect("127.0.0.1", "my_user", "my_password", "my_db");
+
     if (!$link) {
-        die('Ошибка соединения: ' . mysql_error());
+        echo "Ошибка: Невозможно установить соединение с MySQL." . PHP_EOL;
+        echo "Код ошибки errno: " . mysqli_connect_errno() . PHP_EOL;
+        echo "Текст ошибки error: " . mysqli_connect_error() . PHP_EOL;
+        exit;
     }
-    echo 'Успешно соединились';
-    mysql_close($link);
+
+    echo "Соединение с MySQL установлено!" . PHP_EOL;
+    echo "Информация о сервере: " . mysqli_get_host_info($link) . PHP_EOL;
+
+    mysqli_close($link);
 ?>
